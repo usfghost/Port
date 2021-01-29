2ce0f382cb9d1c9df4bb37860746638d697503b4
Yousef Khell <yousef@khell.com>
2021-01-29 19:12:52 +0200

    Added Portfolio Object

--- UStocks.py
+++ UStocks.py
@@ -6,14 +6,50 @@ class UPosition:
         self.quantity = quantity
         self.price = price
         self.value = self.price * self.quantity
+        self.livePrice = self.price
+        self.liveValue = self.livePrice * quantity
+        self.profitLoss = 0
+        self.profitLossPercent = 0
+    
+    def updateData(self):
+        self.livePrice = stock_info.get_live_price(self.ticker)
+        self.liveValue = self.quantity * self.livePrice
+        self.profitLoss = self.liveValue - self.value
+        self.profitLossPercent = (self.liveValue / self.value - 1)*100
 
-    def getUpdatedDataArray(self):
-        livePrice = stock_info.get_live_price(self.ticker)
-        liveValue = self.quantity * livePrice
-        profitLoss = liveValue - self.value
-        profitLossPercent = (liveValue / self.value - 1)*100
-        updatedDataArray = [livePrice, liveValue, "$" + str(profitLoss), str(profitLossPercent) + "%"]
-        return updatedDataArray
+    def toString(self):
+        return str(self.ticker) + "\t" + str(self.quantity) + "\t" + str(self.price) + "\t" + str("{:.2f}".format(self.value)) + "\t" +  "{:.3f}".format(self.livePrice) + "\t\t" + "{:.3f}".format(self.liveValue) + "$\t" + "{:.2f}".format(self.profitLoss) + "\t\t" + "{:.2f}".format(self.profitLossPercent) + "%"
+
 
     def toRowArray(self):
-        return [self.ticker, self.quantity, self.price, self.value]
+        return [self.ticker, self.quantity, "{:.2f}".format(self.price), "{:.2f}".format(self.value), "{:.2f}".format(self.livePrice), "{:.2f}".format(self.liveValue), "$" + "{:.2f}".format(self.profitLoss), "{:.2f}".format(self.profitLossPercent) + "%"]
+
+class UPortfolio:
+    def __init__(self, positions):
+        self.positions = positions
+        self.cost = 0
+        self.value = 0
+        self.updateData()
+
+    def updateData(self):
+        self.cost = 0
+        self.value = 0
+        for i in range(len(self.positions)):
+            self.cost += self.positions[i].value
+            self.value += self.positions[i].liveValue
+            self.profitLoss = self.value - self.cost
+            self.profitLossPercent = (self.value / self.cost - 1)*100
+
+    def toRowArray(self):
+        return[self.cost, self.value, self.profitLoss, self.profitLossPercent]
+
+    def printPortfolio(self):
+        positions = ""
+        for i in range(len(self.positions)):
+            positions += self.positions[i].toString() + "\n"
+        print("//////////////////////////////////////////////////////////////////////////////////")
+        print("Cost\tValue\t\t\tP/L($)\t\t\tP/L(%)")
+        print(str(self.cost) + "\t" + str(self.value) + "\t" + str(self.profitLoss) + "\t" + str(self.profitLossPercent))
+        print("Ticker\tQnty\tPrice\tValue\tLive Price\tLive Value\tP/L($)\t\tP/L(%)")
+        print(positions)
+
--- form_2.py
+++ form_2.py
@@ -3,7 +3,7 @@ from ui.Ui_form_2 import Ui_Form2
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 # UStocks
-from UStocks import UPosition
+from UStocks import UPosition, UPortfolio
 # Raw Package
 import time
 import numpy
@@ -17,8 +17,15 @@ class LoginWindow(qtw.QMainWindow, Ui_Form2):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.positions = []
+        
+        self.portfolio = UPortfolio([
+            UPosition("TSLA",6,548), 
+            UPosition("NIO", 22, 54.13), 
+            UPosition("ARKK", 10, 128.99), 
+            UPosition("NOK", 34, 5.29)
+        ])
+        
+        # self.portfolio.positions = [UPosition("TSLA",6,548), UPosition("NIO", 22, 55), UPosition("ARKK", 10, 129), UPosition("NOK", 34, 5.29)]
 
         self.setupUi(self)
         self.buttonAddPosition.clicked.connect(self.addPosition)
@@ -26,19 +33,16 @@ class LoginWindow(qtw.QMainWindow, Ui_Form2):
         self.initializeTable()
 
     def initializeTable(self):
-        self.tablesheet.setRowCount(4)
-        self.tablesheet.setColumnCount(2)
-        self.tablesheet.setRowCount(10)
-        self.tablesheet.setColumnCount(10)
+        self.tablesheet.setRowCount(5)
+        self.tablesheet.setColumnCount(8)
         rowValues = ["TICKER", "QUANTITY", "BUY PRICE", "BUY VALUE", "LIVE PRICE", "LIVE VALUE", "P/L($)", "P/L(%)"]
         self.printRow(rowValues, 0)
-        for i in range(0, len(self.positions)):
-            positionData = self.positions[i].toRowArray()
-            positionData += self.positions[i].getUpdatedDataArray()
+        for i in range(0, len(self.portfolio.positions)):
+            positionData = self.portfolio.positions[i].toRowArray()
             self.printRow(positionData, i+1)
 
     def startLiveThread(self):
-        t = Thread(target=self._getLivePrice)
+        t = Thread(target=self._getLiveData)
         t.start()
 
     def printRow(self, rowValues, rowIndex):
@@ -50,12 +54,18 @@ class LoginWindow(qtw.QMainWindow, Ui_Form2):
         price = float(self.editBuyPrice.text())
         ticker = self.editTicker.text()
         position = UPosition(ticker, quantity, price)
-        self.positions.append(position)
+        self.portfolio.positions.append(position)
         self.initializeTable()
 
-    def _getLivePrice(self):
+    def _getLiveData(self):
         while(True):
-            self.initializeTable()
+            for i in range(0, len(self.portfolio.positions)):
+                self.portfolio.positions[i].updateData()
+                updatedData = self.portfolio.positions[i].toRowArray()
+                self.printRow(self.portfolio.positions[i].toRowArray(), i+1)
+            self.portfolio.updateData()
+            self.portfolio.toRowArray()
+            self.portfolio.printPortfolio()
 
 if __name__ == '__main__':
     app = qtw.QApplication([])
